18b9e3653cb218532cd02f2aa4170960414ff2bd
Somanshu Agarwal <somanshu@huntsman-ve703-03584.apn.wlan.private.upenn.edu>
2019-11-22 19:08:33 -0500

    Pipeline for CPU and GPU Code

--- src/kernels.cpp
+++ src/kernels.cpp
@@ -1,11 +1,13 @@
 void calculateMahalanobisDistance(glm::vec3 a,glm::vec3 b,float* covar) {
 	float *covarInv = inverse(covar);
-	float dist = (a - b)
+	float dist = (a - b);
 }
 
-void calculatePrior()
+void calculatePrior(){
 
-__global__ void calculateExpectation(int m,float *priorProb) {
+}
+
+__global__ void calculateExpectation(int m,float *priorProb, glm::vec3 *points, glm::vec3 *mean, float *covar) {
 	int index = (blockIdx.x * blockDim.x) + threadIdx.x;
 	if (index >= N)
 		return;
@@ -13,6 +15,6 @@ __global__ void calculateExpectation(int m,float *priorProb) {
 
 	float sum = 0;
 	for (int i = 0; i < m; i++) {
-		sum += calculatePrior(mean[i],covar[i],point[x]);
+		sum += calculatePrior(mean[i],covar[i],points[x]);
 	}
 }
